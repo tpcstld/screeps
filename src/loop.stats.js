5689b80796a9a3cdda7f406b1df01743fcd73da4
Jerry Jiang <jerryjiang1128@gmail.com>
2018-03-19 19:29:54 -0400

    More stats.

--- src/loop.stats.js
+++ src/loop.stats.js
@@ -6,7 +6,12 @@ const loopStats = {
         const containers = utilsFind.findInEveryRoom(FIND_STRUCTURES, {
             filter: s => s.structureType === STRUCTURE_CONTAINER
         });
-        Memory.stats.storedEnergy = _.sum(containers, s => s.store.energy);
+
+        Memory.stats = {
+          storedEnergy: _.sum(containers, s => s.store.energy),
+          cpu: Game.cpu,
+          gcl: Game.gcl,
+        };
     }
 };
 
