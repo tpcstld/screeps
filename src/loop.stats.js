c1caa42d1742c77cf6b21c3e60b3ab7503f18957
Jerry Jiang <jerryjiang1128@gmail.com>
2018-03-20 03:01:50 -0400

    Migrate to per room.

--- src/loop.stats.js
+++ src/loop.stats.js
@@ -3,12 +3,7 @@ const utilsFind = require('utils.find');
 const loopStats = {
 
     run: function() {
-        const containers = utilsFind.findInEveryRoom(FIND_STRUCTURES, {
-            filter: s => s.structureType === STRUCTURE_CONTAINER
-        });
-
         Memory.stats = {
-          storedEnergy: _.sum(containers, s => s.store.energy),
           cpu: {
             bucket: Game.cpu.bucket,
             used: Game.cpu.getUsed()
