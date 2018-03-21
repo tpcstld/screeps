0b425fcd9e79b7e509e2b59f5bd0b4606ed0c92f
Jerry Jiang <jerryjiang1128@gmail.com>
2018-03-21 05:53:20 -0400

    Remove extra.

--- src/role.runner.js
+++ src/role.runner.js
@@ -18,11 +18,11 @@ const roleRunner = {
             utilsLoad.clearTarget(creep);
         } else {
             let target = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
-                filter: r => r.amount >= 40 && r.amount >= (creep.carryCapacity * (utilsLoad.getTargetCount(creep, r, "take") + 1))
+                filter: r => r.amount >= 40 && r.amount >= (creep.carryCapacity * utilsLoad.getTargetCount(creep, r, "take"))
             });
             if (!target) {
               target = creep.pos.findClosestByPath(FIND_TOMBSTONES, {
-                  filter: t => _.sum(t.store) >= Math.max(creep.carryCapacity * (utilsLoad.getTargetCount(creep, t, "take") + 1), 1)
+                  filter: t => _.sum(t.store) >= Math.max(creep.carryCapacity * utilsLoad.getTargetCount(creep, t, "take"), 1)
               });
             }
             if (!target) {
@@ -30,7 +30,7 @@ const roleRunner = {
                   filter: s => s.structureType === STRUCTURE_CONTAINER
               });
               if (containers.length > 0) {
-                containers = _.sortBy(containers, c => -(_.sum(c.store) - (creep.carryCapacity * (utilsLoad.getTargetCount(creep, c, "take") + 1))));
+                containers = _.sortBy(containers, c => -(_.sum(c.store) - (creep.carryCapacity * utilsLoad.getTargetCount(creep, c, "take"))));
                 target = containers[0];
               }
             }
