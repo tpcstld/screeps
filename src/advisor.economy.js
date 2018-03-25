e6849715af234b75d907afd1efbaa39b602a638e
Jerry Jiang <jerryjiang1128@gmail.com>
2018-03-25 00:53:40 -0400

    Add economy advisor.

---
+++ src/advisor.economy.js
@@ -0,0 +1,46 @@
+
+class EconomyAdvisor {
+
+  getNeeds() {
+    let needs = [];
+    for (let name in Game.rooms) {
+      const room = Game.rooms[name];
+      const roomNeeds = this.getNeedsForRoom(room);
+      needs = needs.concat(roomNeeds);
+    }
+
+    return needs;
+  }
+
+  getNeedsForRoom(room) {
+    const needs = [];
+
+    const roomCreeps = room.find(FIND_MY_CREEPS);
+
+    // Every room needs one refiller.
+    const refillers = _.filter(roomCreeps, c => c.role == "refill");
+    if (refillers.length < 0) {
+      needs.push({
+          type: "spawn",
+          role: "refill",
+          roomId: room.id,
+      });
+    }
+
+    // Every room should have as many miners as sources.
+    const sources = room.find(FIND_SOURCES);
+    const miners = _.filter(roomCreeps, c => c.role == "refill");
+    const numDifference = sources.length - miners.length;
+    for (let i = 0; i < numDifference; i++) {
+      needs.push({
+          type: "spawn",
+          role: "mine",
+          roomId: room.id,
+      });
+    }
+
+    return needs;
+  }
+}
+
+module.exports = EconomyAdvisor;
