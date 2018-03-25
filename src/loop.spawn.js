cee626acc7bece34231d6df222d3e9bfcfda16a3
Jerry Jiang <jerryjiang1128@gmail.com>
2018-03-25 02:29:49 -0400

    A

--- src/loop.spawn.js
+++ src/loop.spawn.js
@@ -1,12 +1,10 @@
 const loopSpawn = {
 
     NUM_TYPES: [
-        {type: 'refill', num: (spawn) => 1},
         {type: 'link_refiller', num: (spawn) => spawn.room.find(FIND_STRUCTURES, {filter: s => s.structureType == STRUCTURE_LINK}).length > 0 ? 1 : 0},
         {type: 'invader', refundable: true, num: (spawn) => Math.ceil(_.max(_.map(Memory.stats.rooms, r => r.name != spawn.room.name ? r.numEnemies : 0)) / 5)},
         {type: 'attack', num: (spawn) => 0},
         {type: 'heal', num: (spawn) => 0},
-        {type: 'mine', num: (spawn) => 2},
         {type: 'upgrade', num: (spawn) => 2},
         {type: 'build', refundable: true, num: (spawn) => Math.min(2, Memory.stats.rooms[spawn.room.name].numConstructionSites)},
         {type: 'run', num: (spawn) => {
