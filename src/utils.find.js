dd7a61c8ad3d8f741033e3b24e5c3fdb8bb1af6e
Jerry Jiang <jerryjiang1128@gmail.com>
2018-03-26 18:47:03 -0400

    Find closest by room.

--- src/utils.find.js
+++ src/utils.find.js
@@ -10,7 +10,27 @@ const utilsFind = {
             total = total.concat(targets);
         }
         return total;
-    }
+    },
+
+    findClosestOwnedRoom: function(targetRoom, opts) {
+      if (!opts) {
+        opts = {};
+      }
+
+      if (!opts.filter) {
+        opts.filter = () => true;
+      }
+
+      let rooms = _.filter(Game.rooms, opts.filter);
+      rooms = _.map(rooms, r => {
+          room: r,
+          route: Game.map.findRoute(r, targetRoom)
+      });
+      rooms = _.filter(rooms, r => r.route != ERR_NO_PATH);
+
+      const closestRoom = _.minBy(rooms, r => r.route.length);
+      return closestRoom.room;
+    },
 };
 
 module.exports = utilsFind;
