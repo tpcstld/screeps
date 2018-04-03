118be76511b34506daf27fcdcae5bd080c984274
Jerry Jiang <jerryjiang1128@gmail.com>
2018-04-02 23:18:29 -0400

    Builder.

--- src/role.builder.js
+++ src/role.builder.js
@@ -6,24 +6,49 @@ const constants = require('utils.constants');
 
 let roleBuilder = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
+  getNeed: function(creep, needs) {
+    const target = creep.memory.target;
 
-        if (creep.memory.building && creep.carry.energy == 0) {
-            creep.memory.building = false;
-        }
-        if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.building = true;
-        }
+    if (!creep.memory.working) {
+      return null;
+    }
 
-        if (creep.memory.building) {
-            utilsEnergy.placeEnergy(creep);
-        } else {
-            if (!utilsHarvest.getEnergyFromContainers(creep)) {
-                creep.idle();
-            }
+    if (target) {
+      return _.filter(needs, n => n.type == "build" && n.target == target)[0];
+    }
+
+    const need = _.filter(needs, n => n.type == "build")[0];
+    if (need) {
+      creep.memory.target = need.target;
+    }
+    return need;
+  },
+
+  run: function(creep, need) {
+    if (creep.memory.working && creep.carry.energy == 0) {
+      creep.memory.target = null;
+      creep.memory.working = false;
+    }
+    if (!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.working = true;
+    }
+
+    if (creep.memory.working) {
+      const target = Game.getObjectById(creep.memory.target);
+      if (!target) {
+        creep.memory.target = null;
+        return;
+      }
+
+      if (creep.build(target) == ERR_NOT_IN_RANGE) {
+        creep.travelTo(target);
+      }
+    } else {
+        if (!utilsHarvest.getEnergyFromContainers(creep)) {
+            creep.idle();
         }
     }
+  }
 };
 
 module.exports = roleBuilder;
