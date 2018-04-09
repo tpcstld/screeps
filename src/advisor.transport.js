f35fe94bc7d30f5b71dbba33cdabcfb7b65a1de9
Jerry Jiang <jerryjiang1128@gmail.com>
2018-04-09 01:18:54 -0400

    Add an additional transporter if energy storage is critical.

--- src/advisor.transport.js
+++ src/advisor.transport.js
@@ -32,16 +32,25 @@ const TransportAdvisor = {
     let richRooms = _.filter(Game.rooms, r => r.storage && r.storage.store[RESOURCE_ENERGY] > 250000);
     richRooms = _.sortBy(richRooms, r => -(r.storage.store[RESOURCE_ENERGY]));
 
+    let numTransporters = 1;
+    if (room.storage.store[RESOURCE_ENERGY] < 100000) {
+      numTransporters = 2;
+    }
+
     for (let name in richRooms) {
       const richRoom = richRooms[name];
       const route = Game.map.findRoute(richRoom, room);
       if (route.length < 2) {
-        return [{
+        const output = [];
+        for (let i = 0; i < numTransporters; i++) {
+          output.push({
             type: "transport",
             resource: RESOURCE_ENERGY,
             start: richRoom.storage.id,
             target: room.storage.id
-        }];
+          });
+        }
+        return output;
       }
     }
 
