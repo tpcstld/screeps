24810eed87872eb061b25dcb1fe4b8575a89ab13
Jerry Jiang <jerryjiang1128@gmail.com>
2018-04-11 14:28:20 -0400

    Cache for plain finds.

--- src/prototype.room.js
+++ src/prototype.room.js
@@ -3,6 +3,19 @@ if (!Room.prototype._find) {
   Room.prototype._find = Room.prototype.find;
 
   Room.prototype.find = function(type, opts) {
-    return this._find(type, opts);
+    if (!this.findCache) {
+      this.findCache = {};
+    }
+
+    const cachedResult = this.findCache[type];
+    if (opts == undefined && cachedResult) {
+      return cachedResult;
+    }
+
+    const result = this._find(type, opts);
+    if (opts == undefined) {
+      this.findCache[type] = result;
+    }
+    return result;
   }
 }
